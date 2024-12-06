9bd6225b307719f69927bd709518004191ea27c0
sammy2201 <bhavanish2000@gmail.com>
2024-12-06 21:30:42 +0100

    first commit

---
+++ components/footer.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import styles from "../styles/page.module.css";
+
+export default function Footer() {
+  return (
+    <div>
+      <footer className={styles.footer}>
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer">
+          Learn
+        </a>
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer">
+          Examples
+        </a>
+        <a
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer">
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
+}
