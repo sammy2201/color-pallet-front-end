e3fe61a70670b73cc4543ff2faa7d3a8b82dd00a
sammy2201 <bhavanish2000@gmail.com>
2025-05-01 21:47:23 +0530

    theme change is done

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from "next";
 import "../styles/globals.css";
 import HomePageNavbar from "../components/navbar/homePageNavbar";
 import { Inter } from "next/font/google";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Tone set",
-  description: "Color pallete for website and apps",
+  description: "Color palette for website and apps",
   icons: {
     icon: "/favicon.ico", // path relative to the /public folder
   },
@@ -19,10 +20,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    // <html
+    //   lang="en"
+    //   className="bg-[#FCFCFC] dark:bg-black text-black dark:text-white">
+    //   <body className={`min-h-screen ${inter.className}`}>
+    //     <HomePageNavbar />
+    //     {children}
+    //   </body>
+    // </html>
+
+    <html suppressHydrationWarning lang="en">
+      {/*
+      <head /> will contain the components returned by the nearest parent
+      head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+    */}
+      <head />
+
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
-        <HomePageNavbar />
-        {children}
+        <Providers>
+          <HomePageNavbar />
+          {children}
+        </Providers>
       </body>
     </html>
   );
